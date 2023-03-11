1340cdd221062eeae045c51a4387392765a8aefd
Luis Leguizamón <lleguizamonl@gmail.com>
2023-03-11 20:26:43 -0300

    New: List categories in search page

--- resources/js/Pages/Search/Search.jsx
+++ resources/js/Pages/Search/Search.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+const areas = ['deportes', 'finanzas', 'liderazgo', 'programación'];
+
+const areasNames = [];
+
+areas.forEach((data) => {
+    areasNames.push(<li class="bg-slate-100 m-5 p-5 text-center">{data}</li>)
+})
+
 const Home = () => {
-    return <p>Search</p>;
+    return (
+        <>
+            <div class="grid grid-cols-4 gap-4">
+                <ul class="m-5 p-5">
+                    {areasNames}
+                </ul>
+            </div>
+        </>
+    );
 };
 
 export default Home;
