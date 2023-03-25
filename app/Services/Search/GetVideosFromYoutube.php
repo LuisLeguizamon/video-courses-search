e83217b88ac57edb8c917f58af981bbe725cd615
Luis Leguizamón <lleguizamonl@gmail.com>
2023-03-25 16:31:13 -0300

    Refactor: Add functions to get url and params in service that get videos from youtube

--- app/Services/Search/GetVideosFromYoutube.php
+++ app/Services/Search/GetVideosFromYoutube.php
@@ -6,20 +6,7 @@ class GetVideosFromYoutube
 {
     public function execute(string $category) : array
     {
-        $url = 'https://www.googleapis.com/youtube/v3/search';
-        $params = [
-            'key' => env('YOUTUBE_API_KEY'),
-            'maxResults' => 5,
-            'part' => 'id,snippet',
-            'q' => $category,
-            'type' => 'video',
-            'videoCategoryId' => 27,
-            /* videoCategoryId = 27 for Education.
-            This data have been retrieve from 
-            the "list category" method in the API
-            https://youtube.googleapis.com/youtube/v3/videoCategories?part=id&part=snippet&regionCode=PY&key=[YOUR_API_KEY]*/
-        ];
-        $url .= '?' . http_build_query($params);
+        $url = $this->getUrl($category);
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
@@ -29,6 +16,33 @@ class GetVideosFromYoutube
         curl_close($curl);
 
         return $this->processResponse($response);
+    }
+
+    private function getUrl(string $category)
+    {
+        $url = 'https://www.googleapis.com/youtube/v3/search';
+
+        $params = $this->getParams($category);
+
+        $url .= '?' . http_build_query($params);
+
+        return $url;
+    }
+
+    private function getParams(string $category) : array
+    {
+        return [
+            'key' => env('YOUTUBE_API_KEY'),
+            'maxResults' => 5,
+            'part' => 'id,snippet',
+            'q' => $category,
+            'type' => 'video',
+            'videoCategoryId' => 27,
+            /* videoCategoryId = 27 for Education.
+             This data have been retrieve from the "list category" method in the API
+             https://youtube.googleapis.com/youtube/v3/videoCategories?part=id&part=snippet&regionCode=PY&key=[YOUR_API_KEY]
+            */
+        ];
     }
 
     private function processResponse($response) : array
