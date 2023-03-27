8f5832a51508157d5e4b587984cd6a24e375533b
Luis Leguizamón <lleguizamonl@gmail.com>
2023-03-27 19:31:21 -0400

    Adjust: make the entire button clickeable in results page

--- resources/js/Pages/Search/Results.jsx
+++ resources/js/Pages/Search/Results.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { router } from '@inertiajs/react';
 
 export default function Results(props) {
     const category = props.category;
     const videos = props.videos;
 
+    let goToVideo = (url) => router.get(url);
+
     const videosTitle = videos.map((item) => {
         const url = route("search.show_video", { 'videoId': item.videoId });
         return (
-            <li className="bg-white capitalize mt-5 p-5 h-auto w-full rounded shadow-2xl shadow-gray-500/20"
+            <li onClick={ () => goToVideo(url) }
+                className="bg-white capitalize cursor-pointer mt-5 p-5 h-auto w-full rounded shadow-2xl shadow-gray-500/20"
                 key={item.videoId}>
-                <a href={url}>{item.videoTitle}</a>
+                {item.videoTitle}
             </li>
         );
     });
