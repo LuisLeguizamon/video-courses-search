7ad9e4dea3b7afb416b5f1e206d63b16da594289
Luis Leguizamón <lleguizamonl@gmail.com>
2023-04-21 12:32:53 -0400

    Improvement: Add favorite video when button is clicked

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
 use App\Services\Search\YouTubeVideoSearch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 
@@ -40,6 +43,17 @@ class SearchController extends Controller
 
     public function markVideoAsFavorite(Request $request)
     {
-        dd($request->all());
+        $videoID = $request->video_id;
+        $userId = Auth::user()->id;
+        $favoriteVideo = Favorite::where('video_id', $videoID)->where('user_id', $userId)->first();
+dd($favoriteVideo);
+        if (! isset($favoriteVideo)) {
+            $favorite = new Favorite();
+            $favorite->video_id = $request->video_id;
+            $favorite->user_id = $userId;
+            $favorite->save();
+        } else {
+            // TODO: message that video already been marked as favorite
+        }
     }
 }
