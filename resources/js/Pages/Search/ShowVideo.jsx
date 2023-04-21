b67e82c03df899c8854a149af4af362dc90895e3
Luis Leguizamón <lleguizamonl@gmail.com>
2023-04-21 12:16:41 -0400

    Init: Mark as favorite button in showvideo page

--- resources/js/Pages/Search/ShowVideo.jsx
+++ resources/js/Pages/Search/ShowVideo.jsx
@@ -6,11 +6,28 @@ export default function Results({auth, videoId}) {
 
     const url = "https://www.youtube.com/embed/"+videoId;
 
+    let favoriteContent;
+
+    function markAsFavorite()
+    {
+        alert("mark")
+    }
+
+    if (auth.user) {
+        favoriteContent = 
+            <div className="text-center">
+                <span className="bg-slate-100 border border-transparent px-2 py-3 rounded-md text-sm hover:bg-slate-200 transition ease-in-out duration-300"
+                      onClick={() => markAsFavorite()}>
+                    Mark as Favorite
+                </span>
+            </div>;
+    }
     return (
         <>
             <Header auth={auth}></Header>
             <Head title="video" />
             <div className="grid grid-cols-1 mt-20">
+                {favoriteContent}
                 <div className="m-5">
                     <iframe
                         width="100%"
