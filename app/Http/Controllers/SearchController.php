fc8f2a52f9abd3c12bd11a4de0f81dd57a7bbcc2
Luis Leguizamón <lleguizamonl@gmail.com>
2023-04-23 18:48:38 -0400

    Improvement: Add a background property to the area array (set the background in the backend instead of frontend)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -15,7 +15,11 @@ class SearchController extends Controller
 {
     public function home()
     {
-        $areas = ['sports', 'finance', 'leadership', 'coding'];
+        $areas = [];
+        array_push($areas, ['area' => 'sports', 'background' => 'bg-gradient-to-r from-cyan-500 to-blue-500']);
+        array_push($areas, ['area' => 'finance', 'background' => 'bg-gradient-to-r from-cyan-500 to-emerald-500']);
+        array_push($areas, ['area' => 'leadership', 'background' => 'bg-gradient-to-r from-sky-500 to-blue-800']);
+        array_push($areas, ['area' => 'coding', 'background' => 'bg-gradient-to-r from-blue-500 to-purple-500']);
 
         return Inertia::render('Search/Home', [
             'areas' => $areas,
